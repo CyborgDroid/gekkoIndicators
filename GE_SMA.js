97b84ad80e2b87dbca37b335578e13438be02615
CyborgDroid <glenerikcortes@gmail.com>
2018-05-14 19:16:25 -0400

    Update GE_SMA.js

--- GE_SMA.js
+++ GE_SMA.js
@@ -12,11 +12,10 @@ var Indicator = function(interval) {
 Indicator.prototype.update = function(price) {
   //add price to beginning of array
   this.prices.unshift(price);
-  //get oldest price, if it's greater than the interval, remove it from calculations
   if (!this.beyond_first_complete_run){
     this.beyond_first_complete_run = this.prices.length > this.interval;
     this.sum += price;
-    this.result = (this.sum / this.prices.length).toFixed(8);
+    this.result = (this.sum / this.prices.length);
   } else {
     //add new price, then subtract oldest price and remove it from array
     this.sum += price - this.prices.pop();
